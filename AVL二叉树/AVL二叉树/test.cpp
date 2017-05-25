c94d515383024e9397755af90dde68b3876cfd2e
weierxiao111 <1091868910@qq.com>
2017-05-25 21:45:38 +0800

    AVL树

--- AVL二叉树/AVL二叉树/test.cpp
+++ AVL二叉树/AVL二叉树/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #include <assert.h>
+#include <stdlib.h>
 
 template <class K, class V>
 struct AVLTreeNode
@@ -37,151 +38,160 @@ public:
 			_pRoot = new Node(key, value);
 			return true;
 		}
-		
-		return _Insert(_pRoot, key, value);
+		Node *pCur = _pRoot;
+		Node *parent = NULL;
+		while (pCur)
+		{
+			if (key < pCur->_key)
+			{
+				parent = pCur;
+				pCur = pCur->_pLeft;
+			}
+			else if (key > pCur->_key)
+			{
+				parent = pCur;
+				pCur = pCur->_pRight;
+			}
+			else
+				return false;
+		}
+
+		pCur = new Node(key, value);
+		if (key < parent->_key)
+		{
+			parent->_pLeft = pCur;
+			--parent->_bf;
+		}
+		else
+		{
+			parent->_pRight = pCur;
+			++parent->_bf;
+		}
+		pCur->_parent = parent;
+
+		while (parent && parent->_bf != 0)
+		{
+			if (parent->_bf == 2)
+			{
+				if (pCur->_bf == 1)
+					_RotateL(parent);
+				else
+					_RotateRL(parent);
+				return true;
+			}
+			else if (parent->_bf == -2)
+			{
+				if (pCur->_bf == -1)
+					_RotateR(parent);
+				else
+					_RotateLR(parent);
+				return true;
+			}
+
+			if (parent->_bf == 1)
+			{
+				pCur = parent;
+				parent = parent->_parent;
+				if (parent)
+					++parent->_bf;
+				else
+					return true;
+			}
+			else if (parent->_bf == -1)
+			{
+				pCur = parent;
+				parent = parent->_parent;
+				if (parent)
+					--parent->_bf;
+				else
+					return true;
+			}
+		}
 	}
 
-	void InOrder()
+	bool IsBalance()
+	{
+		return IsBalanceTree(_pRoot);
+	}
+
+ void InOrder()
 	{
 		cout << "中序：";
 		_InOrder(_pRoot);
 		cout << "end" << endl;
 	}
-private:
-	bool _Insert(Node* proot, const K& key, const V& value)
+private:	
+	void  _RotateL(Node* parent)
 	{
-		if (proot->_pLeft == NULL && key<proot->_key)
-		{
-			proot->_pLeft = new Node(key, value);
-			proot->_pLeft->_parent = proot;
-			-- proot->_bf;
-			_Rotate(proot);
-			return true;
-		}
-		if (proot->_pRight == NULL && key > proot->_key)
-		{
-			proot->_pRight = new Node(key, value);
-			proot->_pRight->_parent = proot;
-			++proot->_bf;
-			_Rotate(proot);
-			return true;
-		}
-
-		if (key == proot->_key)
-			return false;
-
-		if (key < proot->_key)
-			return _Insert(proot->_pLeft, key, value);
-		if (key > proot->_key)
-			return _Insert(proot->_pRight, key, value);
-	}
-
-	void _Rotate(Node *proot)
-	{
-		assert(proot);
-		if (0 == proot->_bf)
-			return;
-		while ((proot->_bf != 2) && (proot->_bf != -2))
-		{
-			if (proot == _pRoot)
-				return;
-			proot = proot->_parent;
-
-			if (proot->_bf == 1)
-				++proot->_bf;
-			if (proot->_bf == -1)
-				--proot->_bf;
-		}
-
-		if (proot->_bf == 2)
-		{
-			if (proot->_pRight->_bf == 1)
-				return _RotateL(proot);
-			else
-				return _RotateRL(proot);
-		}
-		else if (proot->_bf == -2)
-		{
-			if (proot->_pLeft->_bf == -1)
-				return _RotateR(proot);
-			else
-				return _RotateLR(proot);
-		}
+		Node *SubR = parent->_pRight;
+		Node *SubRL = SubR->_pLeft;
 		
-	}
+		parent->_pRight = SubRL;
+		if (SubRL != NULL)
+		SubRL->_parent = parent;
 
-	void  _RotateL(Node* parent){
-		Node *SubL = parent->_pRight;
-		Node* LeftNode = SubL->_pLeft;
-		Node *pParent = parent->_parent;
-
-		SubL->_pLeft = parent;
-	
-		parent->_pRight = LeftNode;
-		if (LeftNode != NULL)
-		LeftNode->_parent = parent;
-	
-		  
-		if (NULL == pParent)
-			_pRoot = SubL;
-		else if (pParent->_pLeft == parent)
-		{
-			pParent->_pLeft = SubL;
-			SubL->_parent = pParent;
-		}
-		else
-		{
-			pParent->_pRight = SubL;
-			SubL->_parent = pParent;
-		}
-
-		parent->_parent = SubL;
-		SubL->_bf = 0;
-		parent->_bf = 0;
-	}
-
-	void  _RotateR(Node* parent) {
-		Node *SubR = parent->_pLeft;
-		Node* RightNode = SubR->_pRight;
-		Node *pParent = parent->_parent;
-
-		SubR->_pRight = parent;
-		if (NULL != RightNode)
-		{
-			parent->_pLeft = RightNode;
-			RightNode->_parent = parent;
-		}
-
-		if (NULL == pParent)
-			_pRoot = SubR;
-		else if (pParent->_pLeft == parent)
-		{
-			pParent->_pLeft = SubR;
-			SubR->_parent = pParent;
-		}
-		else
-		{
-			pParent->_pRight = SubR;
-			SubR->_parent = pParent;
-		}
+		Node *pPParent = parent->_parent;
 
 		parent->_parent = SubR;
+		SubR->_pLeft = parent;
+		SubR->_parent = pPParent;
+
+		if (NULL == pPParent)
+			_pRoot = SubR;
+		else if (pPParent->_pLeft == parent)
+		{
+			pPParent->_pLeft = SubR;
+		}
+		else
+		{
+			pPParent->_pRight = SubR;
+		}
+
 		SubR->_bf = 0;
 		parent->_bf = 0;
 	}
 
-	void _RotateRL(Node *proot)
+	void  _RotateR(Node* parent)
 	{
-		_RotateR(proot->_pRight);
-		_RotateL(proot->_pLeft);
-		proot->_bf = -1;
+		Node *SubL = parent->_pLeft;
+		Node *SubLR = SubL->_pRight;
+		parent->_pLeft = SubLR;
+		if (SubLR != NULL)
+			SubLR->_parent = parent;
+
+		Node *pPParent = parent->_parent;
+
+		parent->_parent = SubL;
+		SubL->_pRight = parent;
+		SubL->_parent = pPParent;
+
+		if (NULL == pPParent)
+			_pRoot = SubL;
+		else if (pPParent->_pLeft == parent)
+		{
+			pPParent->_pLeft = SubL;
+		}
+		else
+		{
+			pPParent->_pRight = SubL;
+		}
+
+		SubL->_bf = 0;
+		parent->_bf = 0;
 	}
 
-	void _RotateLR(Node *proot)
+	void _RotateRL(Node *parent)
 	{
-		_RotateL(proot->_pLeft);
-		_RotateR(proot->_pRight);
-		proot->_bf = 1;
+		_RotateR(parent->_pRight);
+		_RotateL(parent);
+		parent->_pRight->_bf = -1;
+	}
+
+	void _RotateLR(Node *parent)
+	{
+		_RotateL(parent->_pLeft);
+		_RotateR(parent);
+		parent->_pLeft->_bf = 1;
 	}
 
 	void _InOrder(Node *proot)
@@ -193,7 +203,34 @@ private:
 		_InOrder(proot->_pRight);
 	}
 
-	
+	int Hight(Node *proot)
+	{
+		if (NULL == proot)
+			return 0;
+		if (proot->_pLeft == NULL && proot->_pRight == NULL)
+			return 1;
+		int LeftHight = Hight(proot->_pLeft);
+		int RightHight = Hight(proot->_pRight);
+		return 1 + ((LeftHight > RightHight) ? LeftHight : RightHight);
+	}
+
+	bool IsBalanceTree(Node *proot)
+	{
+		if (NULL == proot)
+			return true;
+		if (proot->_bf >= 2 || proot->_bf <= -2)
+		{
+			cout << proot->_key<<"平衡因子大于2！" << endl;
+			return false;
+		}
+		if (Hight(proot->_pRight) - Hight(proot->_pLeft) != proot->_bf)
+		{
+			cout <<proot->_key<<"  平衡因子不正确!" << endl;
+			return false;
+		}
+		if (IsBalanceTree(proot->_pLeft) && IsBalanceTree(proot->_pRight))
+			return true;
+	}
 private:
 	Node *_pRoot;
 };
@@ -204,8 +241,9 @@ int main()
 	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i)
 	{
 		avtree.Insert(arr[i], arr[i]);
+		avtree.IsBalance();
 	}
 	avtree.InOrder();
-	system("puase");
+	getchar();
 	return 0;
 }
